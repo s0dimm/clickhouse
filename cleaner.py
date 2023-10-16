ea4669728642e239f71c6830f7332aed96f30b28
s0dimm <57068213+s0dimm@users.noreply.github.com>
2023-10-16 21:32:08 +0300

    Update cleaner.py

--- cleaner.py
+++ cleaner.py
@@ -3,19 +3,31 @@ from clickhouse_driver import Client
 from datetime import datetime, timedelta
 import logging
 import sys
+import os
+
+host = os.environ['HOST']
+port = os.environ['PORT']
+user = os.environ['USER']
+password = os.environ['PASSWORD']
+database = os.environ['DATABASE']
+table = os.environ['TABLE']
 
 root = logging.getLogger()
 root.setLevel(logging.DEBUG)
+
 handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 root.addHandler(handler)
 
-client = Client(host='10.0.0.1', port=30803, user='jaeger', password='ejhfehfhehf', database='jaeger')
-current_time = datetime.now()
-delete_query = f"DELETE FROM jopa WHERE timestamp = 1999"
-client.execute(delete_query)
-query_optimize = 'OPTIMIZE TABLE jopa FINAL'
-client.execute(query_optimize)
-client.disconnect()
+client = Client((host), (port), (database), (user), (password))
+delete = f"DELETE FROM {table} WHERE timestamp < '1999'"
+optimize = f"OPTIMIZE TABLE {table} FINAL"
+
+def clear(client, delete, optimize):
+    client.execute(delete)
+    client.execute(optimize)
+    client.disconnect()
+if __name__ == "__main__":
+    clear(client, delete, optimize)
