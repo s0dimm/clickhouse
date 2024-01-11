13bffc499f28aa264bc5384d23abd32b91157892
s0dimm <57068213+s0dimm@users.noreply.github.com>
2024-01-11 20:31:27 +0300

    Update cleaner.py

--- cleaner.py
+++ cleaner.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from clickhouse_driver import Client
 from datetime import datetime, timedelta
 import logging
@@ -14,7 +13,6 @@ table = os.environ['TABLE']
 
 root = logging.getLogger()
 root.setLevel(logging.DEBUG)
-
 handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -22,12 +20,14 @@ handler.setFormatter(formatter)
 root.addHandler(handler)
 
 client = Client((host), (port), (database), (user), (password))
-delete = f"DELETE FROM {table} WHERE timestamp < '1999'"
+current_time = datetime.now()
+delete = f"DELETE FROM {table} WHERE timestamp < '{current_time.strftime('%Y-%m-%d')}'"
 optimize = f"OPTIMIZE TABLE {table} FINAL"
 
 def clear(client, delete, optimize):
     client.execute(delete)
     client.execute(optimize)
     client.disconnect()
+
 if __name__ == "__main__":
     clear(client, delete, optimize)
